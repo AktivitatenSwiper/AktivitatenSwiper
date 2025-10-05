28c9f5883cbd158a806aa02fde2e85c19bb3bb21
chris <christophleptin@gmail.com>
2025-10-05 11:27:39 +0200

    configure activity interface & add activities

--- src/types/activity.ts
+++ src/types/activity.ts
@@ -1,8 +1,16 @@
 export interface Activity {
     id: number;
     name: string;
-    tags: string[];
-    price: string;
     description: string;
-    image?: string;
+    category: "Kultur" | "Sport" | "Essen & Trinken" | "Outdoor" | "Indoor" | "Entspannung" | "Abenteuer" | "Soziales" | "Sonstige";
+    min_required_time: number;
+    max_required_time: number;
+    min_participants: number;
+    max_participants: number;
+    cost: "Free" | "€" | "€€" | "€€€";
+    location_type: "Indoor" | "Outdoor" | "Both";
+    required_equipment?: string[];
+    seasonal_suitability: ("Spring" | "Summer" | "Autumn" | "Winter" | "Year-round")[];
+    tags?: string[];
+    image: string;
 }
